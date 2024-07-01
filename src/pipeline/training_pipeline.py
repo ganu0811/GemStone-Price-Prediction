68b2ddd2e3a15eabb1d71d0a596c87290b72f2e9
ganu0811 <ganufindsu@gmail.com>
2024-07-01 15:44:07 +0100

    Mlflow added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -8,6 +8,7 @@ import pandas as pd
 from src.components.data_ingestion import DataIngestion
 from src.components.model_training import ModelTrainer
 from src.components.data_transformation import DataTransformation
+from src.components.model_evaluation import ModelEvaluation
 obj = DataIngestion()
 
 train_data_path, test_data_path = obj.initiate_data_ingestion()
@@ -19,4 +20,7 @@ train_arr, test_arr = data_transformation.initiate_data_transformation(train_dat
 model_trainer = ModelTrainer()
 
 model_trainer.initiate_model_training(train_arr, test_arr)
+
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr, test_arr)
 
